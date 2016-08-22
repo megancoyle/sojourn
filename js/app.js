a8c63cb8f86fcb9bb2d1f42c18560cc8c7821fd5
Taylor Edwards <tedwards1884@gmail.com>
2016-08-22 08:46:54 -0400

    forgot a merge

--- js/app.js
+++ js/app.js
@@ -2,32 +2,6 @@
 
 (function(){
   angular
-<<<<<<< HEAD:app.js
   .module("trips", ["ui.router"])
-=======
-    .module("sojourn", [
-      "ui.router"
-    ])
-    .config([
-      "$stateProvider",
-      RouterFunc
-    ])
-
-    function RouterFunc($stateProvider) {
-      $stateProvider
-        .state("tripsIndex", {
-          url: "/trips",
-          templateUrl: "js/trips/index.html",
-          controller: "TripsIndexCtrl",
-          controllerAs: "indexVm"
-        })
-        .state("entryShow", {
-          url: "entryies/:id",
-          templateUrl: "js/trips/show.html",
-          controller: "TripsShowCtrl",
-          controllerAs: "showVm"
-        })
-    }
->>>>>>> ec42342662d74352ad5bdaad8d7ec7b18137790d:js/app.js
 
 })();

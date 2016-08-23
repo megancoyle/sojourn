9668efe9267d96440ef8c9c0d91e5bbaed338312
Ben Brizzi <brizzitime@gmail.com>
2016-08-23 17:52:35 -0400

    moved map api to trip show page

--- app/assets/javascripts/app/welcome.controller.js
+++ app/assets/javascripts/app/welcome.controller.js
@@ -6,14 +6,10 @@
     .controller("WelcomeCtrl", [
       "$state",
       "TripService",
-      "$scope",
-      "$timeout",
       WelcomeCtrlFunc
     ])
 
-
-
-  function WelcomeCtrlFunc($state, TripService, $scope, $timeout, GoogleMapApi){
+  function WelcomeCtrlFunc($state, TripService){
     var indexVm = this;
 
     TripService.query().then(function (trips) {
@@ -29,64 +25,5 @@
         console.error('Error creating trip', err);
       });
     }
-    $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-      $scope.options = {scrollwheel: false};
-      $scope.coordsUpdates = 0;
-      $scope.dynamicMoveCtr = 0;
-
-      $scope.searchbox = {
-        template:'searchbox.tpl.html',
-        events:{
-          places_changed: function (searchBox) {
-            console.log(searchBox.gm_accessors_.places.Od.searchBoxPlaces[0].geometry
-    .viewport.j.H);
-    console.log(searchBox.gm_accessors_.places.Od.searchBoxPlaces[0].geometry
-    .viewport.H.H);
-          }
-        }
-      }
-      $scope.marker = {
-        id: 0,
-        coords: {
-          latitude: 40.1451,
-          longitude: -99.6680
-        },
-        options: { draggable: true },
-        events: {
-          dragend: function (marker, eventName, args) {
-            $log.log('marker dragend');
-            var lat = marker.getPosition().lat();
-            var lon = marker.getPosition().lng();
-            $log.log(lat);
-            $log.log(lon);
-
-            $scope.marker.options = {
-              draggable: true,
-              labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-              labelAnchor: "100 0",
-              labelClass: "marker-labels"
-            };
-          }
-        }
-      };
-      $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
-        if (_.isEqual(newVal, oldVal))
-        return;
-        $scope.coordsUpdates++;
-      });
-      $timeout(function () {
-        $scope.marker.coords = {
-          latitude: 42.1451,
-          longitude: -100.6680
-        };
-        $scope.dynamicMoveCtr++;
-        $timeout(function () {
-          $scope.marker.coords = {
-            latitude: 43.1451,
-            longitude: -102.6680
-          };
-          $scope.dynamicMoveCtr++;
-        }, 2000);
-      }, 1000);
   }
 })();

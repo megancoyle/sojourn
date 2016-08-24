c1737ff6a069353e2db027120aa77d85d59def87
Taylor Edwards <tedwards1884@gmail.com>
2016-08-24 13:10:11 -0400

    possibly functional gps service

--- app/assets/javascripts/app/welcome.controller.js
+++ app/assets/javascripts/app/welcome.controller.js
@@ -8,10 +8,11 @@
     "$timeout",
     "$state",
     "TripService",
+    "GpsService",
     WelcomeCtrlFunc
   ])
 
-  function WelcomeCtrlFunc($scope, $timeout, $state, TripService){
+  function WelcomeCtrlFunc($scope, $timeout, $state, TripService, GpsService){
     var indexVm = this;
 
     TripService.query().then(function (trips) {
@@ -39,57 +40,57 @@
       template:'searchbox.tpl.html',
       parentDiv: 'searchBoxParent',
       events:{
-        places_changed: function (searchBox) {
-          console.log(searchBox.gm_accessors_.places.Od.searchBoxPlaces[0].geometry
-            .viewport.j.H);
-            console.log(searchBox.gm_accessors_.places.Od.searchBoxPlaces[0].geometry
-              .viewport.H.H);
-            }
+        places_changed: function(searchBox){
+          GpsService.setLatitude(searchBox);
+          GpsService.setLongitude(searchBox);
+          console.log(GpsService.getLatitude());
+          console.log(GpsService.getLongitude());
           }
         }
-        $scope.marker = {
-          id: 0,
-          coords: {
-            latitude: 40.1451,
-            longitude: -99.6680
-          },
-          options: { draggable: true },
-          events: {
-            dragend: function (marker, eventName, args) {
-              $log.log('marker dragend');
-              var lat = marker.getPosition().lat();
-              var lon = marker.getPosition().lng();
-              $log.log(lat);
-              $log.log(lon);
+      }
+      $scope.marker = {
+        id: 0,
+        coords: {
+          latitude: 40.1451,
+          longitude: -99.6680
+        },
+        options: { draggable: true },
+        events: {
+          dragend: function (marker, eventName, args) {
+            $log.log('marker dragend');
+            var lat = marker.getPosition().lat();
+            var lon = marker.getPosition().lng();
+            $log.log(lat);
+            $log.log(lon);
 
-              $scope.marker.options = {
-                draggable: true,
-                labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-                labelAnchor: "100 0",
-                labelClass: "marker-labels"
-              };
-            }
+            $scope.marker.options = {
+              draggable: true,
+              labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+              labelAnchor: "100 0",
+              labelClass: "marker-labels"
+            };
           }
+        }
+      };
+      $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
+        if (_.isEqual(newVal, oldVal))
+        return;
+        $scope.coordsUpdates++;
+      });
+      $timeout(function () {
+        $scope.marker.coords = {
+          latitude: 42.1451,
+          longitude: -100.6680
         };
-        $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
-          if (_.isEqual(newVal, oldVal))
-          return;
-          $scope.coordsUpdates++;
-        });
+        $scope.dynamicMoveCtr++;
         $timeout(function () {
           $scope.marker.coords = {
-            latitude: 42.1451,
-            longitude: -100.6680
+            latitude: 43.1451,
+            longitude: -102.6680
           };
           $scope.dynamicMoveCtr++;
-          $timeout(function () {
-            $scope.marker.coords = {
-              latitude: 43.1451,
-              longitude: -102.6680
-            };
-            $scope.dynamicMoveCtr++;
-          }, 2000);
-        }, 1000);
+        }, 2000);
+      }, 1000);
 
-      }
-    })();
+    }
+  })();

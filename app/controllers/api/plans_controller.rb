a3634765b75c1664485caf10a1b1836358832e88
Megan Coyle <meg@mcoyle.com>
2016-08-25 10:35:31 -0400

    Revert controller changes

--- app/controllers/api/plans_controller.rb
+++ app/controllers/api/plans_controller.rb
@@ -1,6 +1,4 @@
 class Api::PlansController < ApplicationController
-  before_action :cors_preflight_check
-  after_action :cors_set_access_control_headers
 
   def index
     @plans = Plan.all
@@ -50,18 +48,5 @@ class Api::PlansController < ApplicationController
     def plan_params
       params.require(:plan).permit(:name, :weather, :possible_events, :notes)
     end
-
-  protected
-  def cors_preflight_check
-    if request.method == 'OPTIONS'
-      headers['Access-Control-Allow-Origin'] = '*'
-      # ...
-    end
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    # ...
-  end
 
 end

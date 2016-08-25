fa62b410d81c28901b54e6d5d949c07388babd3d
Ben Brizzi <brizzitime@gmail.com>
2016-08-25 10:57:35 -0400

    map style

--- app/assets/javascripts/app/show.controller.js
+++ app/assets/javascripts/app/show.controller.js
@@ -25,42 +25,127 @@
     );
   }
   // map setup
-  $scope.map = {center: {latitude: GpsService.getLatitude(), longitude: GpsService.getLongitude() }, zoom: 10 };
+  $scope.map = {center: {latitude: GpsService.getLatitude(), longitude: GpsService.getLongitude() }, zoom: 12 };
+
   $scope.options = {scrollwheel: false};
   $scope.coordsUpdates = 0;
   $scope.dynamicMoveCtr = 0;
 
-      $scope.marker = {
-        id: 0,
-        coords: {
-          latitude: GpsService.getLatitude(),
-          longitude: GpsService.getLongitude()
-        },
-        options: { draggable: true },
-        events: {
-          dragend: function (marker, eventName, args) {
-            $log.log('marker dragend');
-            var lat = marker.getPosition().lat();
-            var lon = marker.getPosition().lng();
-            $log.log(lat);
-            $log.log(lon);
-
-            $scope.marker.options = {
-              draggable: true,
-              labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-              labelAnchor: "100 0",
-              labelClass: "marker-labels"
-            };
-          }
+  var styleArray = [
+    {
+      "featureType": "administrative",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#444444"
         }
-      };
-      $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
-        if (_.isEqual(newVal, oldVal))
-        return;
-        $scope.coordsUpdates++;
-      });
-    };
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [
+        {
+          "color": "#f2f2f2"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 45
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "simplified"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [
+        {
+          "color": "#6996bf"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    }
+  ];
+  $scope.options = {
+    styles: styleArray
+  };
 
-    TripsShowCtrl.$inject = ['$stateParams', '$state', 'TripService', 'GpsService', "$scope", "$timeout"];
-    angular.module("sojourn").controller("TripsShowCtrl", TripsShowCtrl);
-  })();
+  $scope.marker = {
+    id: 0,
+    coords: {
+      latitude: GpsService.getLatitude(),
+      longitude: GpsService.getLongitude()
+    },
+    options: { draggable: true },
+    events: {
+      dragend: function (marker, eventName, args) {
+        $log.log('marker dragend');
+        var lat = marker.getPosition().lat();
+        var lon = marker.getPosition().lng();
+        $log.log(lat);
+        $log.log(lon);
+
+        $scope.marker.options = {
+          draggable: true,
+          labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+          labelAnchor: "100 0",
+          labelClass: "marker-labels"
+        };
+      }
+    }
+  };
+  $scope.$watchCollection("marker.coords", function (newVal, oldVal) {
+    if (_.isEqual(newVal, oldVal))
+    return;
+    $scope.coordsUpdates++;
+  });
+};
+
+TripsShowCtrl.$inject = ['$stateParams', '$state', 'TripService', 'GpsService', "$scope", "$timeout"];
+angular.module("sojourn").controller("TripsShowCtrl", TripsShowCtrl);
+})();

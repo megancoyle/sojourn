dd39c9cbdb567cdf940c3a46bf0130872d1b3360
Megan Coyle <meg@mcoyle.com>
2016-08-25 09:11:31 -0400

    Fix production error

--- app/assets/javascripts/app/plan-index.controller.js
+++ app/assets/javascripts/app/plan-index.controller.js
@@ -15,7 +15,11 @@
     PlanService.query().then(function (plans) {
       indexVm.plans = plans;
     });
-    // when creating plan, redirect to show page
+
+    indexVm.newPlan = {};
+    indexVm.newPlan.start = new Date();
+    indexVm.newPlan.end = new Date();
+
     indexVm.create = function(){
       PlanService.create(indexVm.newPlan).then(function (plan) {
         $state.go("planShow", {id: plan.id});
